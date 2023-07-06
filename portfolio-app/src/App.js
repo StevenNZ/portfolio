d604c936c94064a9c0e426a290ed09a124ae0446
StevenNZ <listevennz@gmail.com>
2023-07-06 14:29:08 +1200

    feat: implemented horizontal padding to main div

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+
+      <div className="px-[10%]">
+        <Hero />
+      </div>
     </div>
   );
 }
