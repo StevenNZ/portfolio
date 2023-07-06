02ee15ace811d3413310286119e0b4a25fddeedc
StevenNZ <listevennz@gmail.com>
2023-07-06 14:42:14 +1200

    refactor: changed hamburger menu responsiveness from small screen to medium screen

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -11,20 +11,20 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-8 text-black">
       <h1 className="w-full text-3xl font-bold text-[#00df9b]">Steven Li</h1>
-      <ul className="hidden sm:flex">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">About</li>
         <li className="p-4">Projects</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div onClick={handleNav} className="fixed left-[90%] sm:hidden">
+      <div onClick={handleNav} className="fixed left-[90%] md:hidden">
         <AiOutlineMenu size={30} />
       </div>
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-full h-full bg-white ease-in-out duration-500 sm:hidden"
-            : "fixed left-[-100%] top-0 w-full h-full ease-in-out duration-500 sm:hidden"
+            ? "fixed left-0 top-0 w-full h-full bg-white ease-in-out duration-500 md:hidden"
+            : "fixed left-[-100%] top-0 w-full h-full ease-in-out duration-500 md:hidden"
         }
       >
         <div
