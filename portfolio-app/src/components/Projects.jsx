f3d1fc7a7a3359d1d7797df4c91fe3e776730454
StevenNZ <listevennz@gmail.com>
2023-07-07 02:03:58 +1200

    feat: initial projects component implemented

---
+++ portfolio-app/src/components/Projects.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { projectLists } from "../data";
+
+const Projects = () => {
+  return (
+    <div>
+      <img src={projectLists[3].image} alt="no" />
+    </div>
+  );
+};
+
+export default Projects;
