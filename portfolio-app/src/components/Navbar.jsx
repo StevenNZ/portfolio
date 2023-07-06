0eae8be69024220b0042cc1b75e6a4fbf9d731d1
StevenNZ <listevennz@gmail.com>
2023-07-06 14:38:21 +1200

    refactor: default background changed from dark to white mode

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-8 text-white">
+    <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-8 text-black">
       <h1 className="w-full text-3xl font-bold text-[#00df9b]">Steven Li</h1>
       <ul className="hidden sm:flex">
         <li className="p-4">Home</li>
@@ -23,7 +23,7 @@ const Navbar = () => {
       <div
         className={
           !nav
-            ? "fixed left-0 top-0 w-full h-full bg-[#121212] ease-in-out duration-500 sm:hidden"
+            ? "fixed left-0 top-0 w-full h-full bg-white ease-in-out duration-500 sm:hidden"
             : "fixed left-[-100%] top-0 w-full h-full ease-in-out duration-500 sm:hidden"
         }
       >
