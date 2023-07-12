0a796645873c877ed3d321649a7c464d661c5408
StevenNZ <listevennz@gmail.com>
2023-07-13 03:09:56 +1200

    refactor: about me section updated

--- portfolio-app/src/components/About.jsx
+++ portfolio-app/src/components/About.jsx
@@ -5,22 +5,22 @@ import Typed from "react-typed";
 const About = () => {
   return (
     <section id="about" className="bg-white">
-      <div className="max-w-[1000px] w-10/12 h-[calc(100vh-96px)] mx-auto flex justify-center flex-col text-left lg:flex-row lg:items-center">
+      <div className="max-w-[1000px] w-10/12 h-full py-28 mx-auto flex justify-center flex-col text-left lg:flex-row lg:items-center">
         <img
           src={picture}
           alt="Steven Li"
-          className="w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] md:w-[350px] md:h-[350px] mx-auto"
+          className="w-[400px] mx-auto object-contain"
         />
         <div className="lg:ml-12">
-          <p className=" text-xl text-center sm:text-3xl md:text-4xl font-bold pb-6">
+          <p className=" text-xl text-center sm:text-3xl md:text-4xl font-bold py-6 lg:text-start">
             About Me 👨‍💻
           </p>
-          <div className="text-sm sm:text-base md:text-lg text-gray-700">
+          <div className="text-gray-700">
             <p>
               Based in Auckland, New Zealand, I'm a penultimate-year software
               engineering student passionate about pursuing a career as a
               full-stack developer. With a strong foundation in software
-              engineering principles and a drive for continuous learning, I am
+              engineering principles and a drive for continuous learning, I'm
               dedicated to honing my skills and utilising the latest
               technologies. Some well versed technical skills I possess are
               Java, React, and Javascript as shown in my projects. I'm eager to
