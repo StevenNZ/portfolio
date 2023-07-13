3232ced222aa27af5080c9593fc6d6cf7292ae2a
StevenNZ <listevennz@gmail.com>
2023-07-13 16:40:25 +1200

    feat: dark mode added for nav bar

--- portfolio-app/src/components/Navbar.jsx
+++ portfolio-app/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FaSun } from "react-icons/fa";
 import { HashLink as Link } from "react-router-hash-link";
 
-const Navbar = ({ toggleDarkMode }) => {
+const Navbar = ({ darkMode, toggleDarkMode }) => {
   const [nav, setNav] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [visible, setVisible] = useState(true);
@@ -31,12 +32,21 @@ const Navbar = ({ toggleDarkMode }) => {
         visible ? "opacity-100" : "opacity-0 -translate-y-full"
       }`}
     >
-      <div className="flex fixed justify-between items-center h-24 w-full mx-auto px-8 text-black bg-white shadow-md">
-        <h1 className="w-auto text-3xl font-bold mr-8">Steven Li</h1>
-        <BsFillMoonStarsFill
-          className="text-2xl ml-auto md:mr-auto md:ml-0 dark:text-white"
-          onClick={toggleDarkMode}
-        />
+      <div className="flex fixed justify-between items-center h-24 w-full mx-auto px-8 text-black bg-white shadow-md dark:bg-[#212121] dark:text-[#e0e0e0]">
+        <h1 className="w-auto text-3xl font-bold mr-8 tracking-wider">
+          Steven Li
+        </h1>
+        {!darkMode ? (
+          <BsFillMoonStarsFill
+            className="text-2xl ml-auto md:mr-auto md:ml-0"
+            onClick={toggleDarkMode}
+          />
+        ) : (
+          <FaSun
+            className="text-3xl ml-auto md:mr-auto md:ml-0 text-white"
+            onClick={toggleDarkMode}
+          />
+        )}
         <ul className="hidden md:flex md:text-xl md:font-medium">
           <li className="p-4">
             <Link smooth to="#home">
@@ -60,12 +70,12 @@ const Navbar = ({ toggleDarkMode }) => {
           </li>
         </ul>
         <div onClick={handleNav} className="flex left-[90%] md:hidden ml-8">
-          <AiOutlineMenu size={30} />
+          <AiOutlineMenu size={30} className="dark:text-[#e0e0e0]" />
         </div>
         <div
           className={
             !nav
-              ? "fixed left-0 top-0 w-full h-full bg-white ease-in-out duration-500 md:hidden"
+              ? "fixed left-0 top-0 w-full h-full bg-white ease-in-out duration-500 md:hidden dark:bg-[#121212]"
               : "fixed left-[-100%] top-0 w-full h-full ease-in-out duration-500 md:hidden"
           }
         >
