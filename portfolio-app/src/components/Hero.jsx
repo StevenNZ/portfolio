4a0d296d8f4e9d0104c0ef5a7becffe6050c74f1
StevenNZ <listevennz@gmail.com>
2023-07-13 16:48:54 +1200

    feat: dark mode added to hero section

--- portfolio-app/src/components/Hero.jsx
+++ portfolio-app/src/components/Hero.jsx
@@ -11,18 +11,18 @@ const Hero = () => {
         <img
           src={picture}
           alt="Steven Li"
-          className="w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] md:w-[350px] md:h-[350px] mx-auto mb-7 border-2 border-black rounded-full order-1 lg:order-2"
+          className="w-[250px] h-[250px] sm:w-[300px] sm:h-[300px] md:w-[350px] md:h-[350px] mx-auto mb-7 border-2 border-black rounded-full order-1 lg:order-2 dark:border-[#e0e0e0]"
         />
         <div className="order-2 lg:order-1 lg:mr-20">
-          <p className=" text-2xl sm:text-3xl md:text-4xl font-bold pb-6">
+          <h1 className=" text-2xl sm:text-3xl md:text-4xl font-bold pb-6">
             Software Engineering Student 👋
-          </p>
-          <p className="sm:text-lg md:text-xl text-gray-700">
+          </h1>
+          <p className="sm:text-lg md:text-xl text-gray-500">
             Hi, I'm Steven Li. A passionate software engineering student with a
             knack for programming and problem-solving. Explore my projects to
             see my skills and let's connect to discuss exciting opportunities.
           </p>
-          <div className="text-4xl flex justify-center gap-5 py-6">
+          <div className="text-4xl flex justify-center gap-5 py-6 dark:text-[#e0e0e0]">
             <a
               href="https://www.linkedin.com/in/steven-li-/"
               target="_blank"
@@ -42,15 +42,15 @@ const Hero = () => {
       </div>
       <div className="max-w-[1000px] w-10/12 h-full pb-[106px] pt-[50px] mx-auto flex justify-center flex-col text-center lg:flex-row lg:items-center lg:justify-start">
         <div className="w-full flex justify-center lg:w-auto">
-          <p className="text-xl w-36 pb-2 border-b-2 border-gray-300 lg:border-0 lg:mr-4">
+          <h2 className="text-xl w-36 pb-2 border-b-2 border-gray-300 lg:border-0 lg:mr-4 dark:border-gray-600">
             Technical Skills
-          </p>
+          </h2>
         </div>
         <ul className="flex flex-wrap justify-center py-2">
           {languages.map((language, index) => {
             return (
               <li
-                className="m-2 p-4 rounded-full bg-white shadow-2xl"
+                className="m-2 p-4 rounded-full bg-white shadow-2xl dark:bg-gray-700"
                 key={index}
                 data-tooltip-id={index + 1}
                 data-tooltip-content={language.lang}
